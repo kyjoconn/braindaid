e41dc2c3cd0704111de9d6e3f42051ccec290502
Kyle OConnor <kyjoconn@gmail.com>
2022-10-13 19:52:38 -0700

    cleanup

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -9,18 +9,6 @@ export default async function handler(
 ) {
   const client = new DynamoDBClient({});
 
-  // var params = {
-  //   TableName: 'article',
-  //   Key: {
-  //     'title_date_source': { : 'New collections_December 20, 2015_benmvp'}
-  //   }
-  // };
-
-  var params = {
-    //KeyConditionExpression: 'Season = :s and Episode > :e',
-    TableName: 'article'
-  };
-
   let {Items} = await client.send(
     new ScanCommand({
       TableName: 'article',
@@ -45,71 +33,4 @@ return res.status(200).json({
   data: resData
 })
 
-  console.log(Items);
-
-//  ddb.scan(params, function (err, data) {
-//     if (err) {
-//       console.log("Error", err);
-//     } else {
-//       console.log("Success", data);
-//       // data.Items.forEach(function (element, index, array) {
-//       //   console.log(
-//       //       "printing",
-//       //       element.Title.S + " (" + element.Subtitle.S + ")"
-//       //   );
-//       // });
-//       console.log(data.Items[0])
-
-//       resData = data.Items.map((item) => {
-//         return {
-//           uuid: item.title_date_source.S,
-//           source: item.source.S,
-//           title: item.title.S,
-//           image_link: item.image_link.S,
-//           article_link: item.article_link.S
-//         }
-//       })
-
-//       return res.status(200).json({
-//         success: true,
-//         message: "",
-//         data: resData || []
-//       })
-
-//     }
-//   });
-
-
-  // ddb.getItem(params, function(err, data) {
-  //   if (err) {
-  //     console.log("Error", err);
-  //   } else {
-  //     console.log("Success", data.Item);
-  //   }
-  // });
-
-
-
-
-  const mockData: Article[] = [
-    {
-      uuid: "dfskjhfbwjkflsdnf;ldskf",
-      source: "benmvp",
-      title: "How to grate cheese",
-      image_link: "https://i.imgur.com/AD3MbBi.jpeg",
-      article_link: "https://www.benmvp.com/blog/mock-all-functions-module-except-one-jest/",
-    },
-    {
-      uuid: "dfskjhfbwjkflgjsalkdmf'dskfds",
-      source: "benmvp",
-      title: "How to grate pasta",
-      image_link: "https://i.imgur.com/AD3MbBi.jpeg",
-      article_link: "https://www.benmvp.com/blog/mock-all-functions-module-except-one-jest/",
-    },
-  ]
-  // res.status(200).json({
-  //   success: true,
-  //   message: "",
-  //   data: mockData
-  // })
 }
